d5e07bd437aff0d2e9f170b9c53fee3ec769d2b9
BP-linlan <BP-linlan@BASENP20-560>
2024-03-27 13:42:00 +0900

    Add basic CS framework to the project

---
+++ ClientIdGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ETCRegionManagementSimulator
+{
+    public static class ClientIdGenerator
+    {
+        private const string clientId1 = "DATA_UPSTREAM_LINE";
+        private const string clientId2 = "SERVER_HEALTH_CHECK";
+        private const string clientId3 = "DATA_DOWNSTREAM_LINE";
+        private const string clientId4 = "CLIENT_HEALTH_CHECK";
+        private const string clientId5 = "CONTROL_LINE";
+
+        private static Stack<string> IdPool = new Stack<string>();
+
+        static ClientIdGenerator()
+        {
+            IdPool.Push(clientId1);
+            IdPool.Push(clientId2);
+            IdPool.Push(clientId3);
+            IdPool.Push(clientId4);
+            IdPool.Push(clientId5);
+        }
+        public static string GenerateClientId()
+        {
+            return IdPool.Pop();
+        }
+
+        public static string GenerateClientUuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
--- Server.cs
+++ Server.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading.Tasks;
+
 
 namespace ETCRegionManagementSimulator
 {
@@ -14,14 +14,16 @@ namespace ETCRegionManagementSimulator
         private IPAddress defaultIPAddress;
         private IPAddress backupIPAddress;
         private List<int> ports;
-        private TcpListener listener;
+        private List<TcpListener> listeners;
         private bool running;
+        private ClientsManager clientManager;
 
         public Server(string defaultIP, string backupIP, List<int> ports)
         {
             defaultIPAddress = IPAddress.Parse(defaultIP);
             backupIPAddress = IPAddress.Parse(backupIP);
             this.ports = ports;
+            clientManager = new ClientsManager();
             running = false;
         }
 
@@ -30,8 +32,46 @@ namespace ETCRegionManagementSimulator
             if (!running)
             {
                 running = true;
-                listener = new TcpListener(defaultIPAddress, 0);  //Set port number to default 0 to initialize the listener  
+                foreach(int port in ports)
+                { 
+                    TcpListener listener = new TcpListener(defaultIPAddress, port);  //Set port number to default 0 to initialize the listener  
+                    listener.Start();
+                    listeners.Add(listener);
+                    Console.WriteLine($"Server started on port {port}");
+
+                    // Start accepting clients Asynchronously
+                    await AcceptClientsAsync(listener, port);
+                }
             }
+        }
+
+        private async Task AcceptClientsAsync(TcpListener listener, int port)
+        {
+            while (running)
+            {
+                try
+                {
+                    TcpClient client = await listener.AcceptTcpClientAsync();
+
+                    Console.WriteLine($"Connection established on port {port}");
+
+                    await Task.Run(() => HandleClientAsync(client));
+                }
+                catch(SocketException ex)
+                {
+                    Console.WriteLine($"Error accepting client connection on port {port} : {ex.Message}");
+                }
+            }
+        }
+
+        private async Task HandleClientAsync(TcpClient tcpClient)
+        {
+            string clientId = ClientIdGenerator.GenerateClientId();
+            Client client = new Client(clientId, tcpClient);
+
+            clientManager.AddClient(clientId, client);
+
+            client.ReadData();
         }
     }
 }
