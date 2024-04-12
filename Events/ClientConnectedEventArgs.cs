918fa90b0d6020004b42a3448edce5717c31c8ec
BP-linlan <BP-linlan@BASENP20-560>
2024-04-12 13:46:33 +0900

    Implement the send excel row data function: 1. Add eventArgs 2. register the event handler 3. modify the server read and send data process logic; separate those two method into individual task. 4. still something need to be done in ContinuouslyReadData method to insure graceful exit when client get disonnected

---
+++ Events/ClientConnectedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETCRegionManagementSimulator.Events
+{
+    public class ClientConnectedEventArgs : EventArgs
+    {
+        public string ClientId { get; }
+        public ClientConnectedEventArgs(string clientId)
+        {
+            ClientId = clientId;
+        }
+
+    }
+}
---
+++ Events/SendSelectedDataEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETCRegionManagementSimulator.Events
+{
+    public class SendSelectedDataEventArgs : EventArgs
+    {
+        public string SelectedData { get; }
+
+        public int SelectedRowIndex { get; }
+        public SendSelectedDataEventArgs(string selectedData)
+        {
+            SelectedData = selectedData;
+        }
+        public SendSelectedDataEventArgs(int selectedIndex)
+        {
+            SelectedRowIndex = selectedIndex;
+        }
+        public SendSelectedDataEventArgs(int selectedIndex, string selectedData)
+        {
+            SelectedRowIndex = selectedIndex;
+            SelectedData = selectedData;
+        }
+    }
+}
